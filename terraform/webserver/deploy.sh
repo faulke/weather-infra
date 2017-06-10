2466312ffd59d7940c1e46c5fecd739896278f3f
faulke <faulkev@gmail.com>
2017-06-10 14:17:26 -0600

    Add access key variables

--- terraform/webserver/deploy.sh
+++ terraform/webserver/deploy.sh
@@ -3,5 +3,5 @@ echo $1
 export AMI="$(aws ec2 describe-images --filters Name=tag:Version,Values=$1 --query Images[0].[ImageId] --output text)"
 echo $AMI
 
-terraform plan -var ami=$AMI
+terraform plan -var ami=$AMI -var access_key=$AWS_ACCESS_KEY_ID -var secret_key=$AWS_SECRET_ACCESS_KEY
 terraform apply -var ami=$AMI
