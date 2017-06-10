455f4ddf5e5a67afbd4f91fbba87e694e02b16b2
Evan Faulk <faulkev@gmail.com>
2017-06-10 17:07:35 -0600

    Autoscaling group deploys with terraform (#1)
    * Test vpc settings for packer
    
    * Add public ip address to packer instance
    
    * Change filename
    
    * Set version variable for deploy build config
    
    * Add terraform deploy script
    
    * Update route table id
    
    * Add autoscaling group
    
    * Add access key variables
    
    * Apply aws credentials as parameters
    
    * test setting env vars
    
    * Add s3 backend
    
    * add access keys for terraform plan
    
    * Add loop for getting AMI id
    
    * get verion
    
    * get version
    
    * remove version check

---
+++ terraform/webserver/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+echo $1
+
+# initialize ami id variable
+AMI="None"
+
+# try getting AMI id until it is available
+until [ $AMI != "None" ]; do
+  echo "Fetching AMI."
+  AMI="$(aws ec2 describe-images --filters Name=tag:Version,Values=$1 --query Images[0].[ImageId] --output text)"
+  done
+
+echo $AMI
+
+# get tfstate from s3 backend
+terraform init
+
+# validate plan
+terraform plan -var ami=$AMI -var access_key=$AWS_ACCESS_KEY_ID -var secret_key=$AWS_SECRET_ACCESS_KEY
+
+# apply plan -- deploy
+terraform apply -var ami=$AMI -var access_key=$AWS_ACCESS_KEY_ID -var secret_key=$AWS_SECRET_ACCESS_KEY
