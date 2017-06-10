657dbd5f2ccbf304b506a55593279fe267c8ddf0
faulke <faulkev@gmail.com>
2017-06-10 14:40:26 -0600

    Apply aws credentials as parameters

--- terraform/webserver/deploy.sh
+++ terraform/webserver/deploy.sh
@@ -3,5 +3,5 @@ echo $1
 export AMI="$(aws ec2 describe-images --filters Name=tag:Version,Values=$1 --query Images[0].[ImageId] --output text)"
 echo $AMI
 
-terraform plan -var ami=$AMI -var access_key=$AWS_ACCESS_KEY_ID -var secret_key=$AWS_SECRET_ACCESS_KEY
+terraform plan -var ami=$AMI -var access_key=$2 -var secret_key=$3
 terraform apply -var ami=$AMI
