04c2d1cb5ef5061c1a202690929f1459dbd3d620
faulke <faulkev@gmail.com>
2017-06-10 14:54:54 -0600

    test setting env vars

--- terraform/webserver/deploy.sh
+++ terraform/webserver/deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 echo $1
 export AMI="$(aws ec2 describe-images --filters Name=tag:Version,Values=$1 --query Images[0].[ImageId] --output text)"
+export AWS_ACCESS_KEY=$2
+export AWS_SECRET_KEY=$3
 echo $AMI
 
-terraform plan -var ami=$AMI -var access_key=$2 -var secret_key=$3
+
+terraform plan -var ami=$AMI -var access_key=$AWS_ACCESS_KEY -var secret_key=$AWS_SECRET_KEY
 terraform apply -var ami=$AMI
