0e8682635397e8ad8961965e20dc0fc7058b6683
faulke <faulkev@gmail.com>
2017-06-11 15:13:12 -0600

    Add separate env files for staging and prod

--- terraform/staging/user-data.sh
+++ terraform/staging/user-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo npm install forever -g
+sudo aws s3 cp s3://envtester/staging/.env .env
 cd /home/ec2-user/weatherapp/
 sudo forever start -c "node -r babel-register" tools/distServer.js > /dev/null 2> /dev/null < /dev/null &
