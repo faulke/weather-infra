5167d9f68cce4bfc98121f461e887ee9743e666e
faulke <faulkev@gmail.com>
2017-06-11 16:02:56 -0600

    Get .env file on launch

--- terraform/staging/user-data.sh
+++ terraform/staging/user-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo aws s3 cp s3://envtester/staging/.env .env
 cd /home/ec2-user/weatherapp/
+sudo aws s3 cp s3://envtester/staging/.env .env
 sudo forever start -c "node -r babel-register" tools/distServer.js > /dev/null 2> /dev/null < /dev/null &
